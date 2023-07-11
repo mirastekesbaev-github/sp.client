6619640e12783ca41c13414d1212548e40b1a422
miras-pc <mirastekesbaev@gmail.com>
2023-07-11 19:21:33 +0600

    initial commit

---
+++ src/app/index.tsx
@@ -0,0 +1,9 @@
+function Index() {
+    return (
+        <div className="App">
+
+        </div>
+    );
+}
+
+export default Index;
--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './app/index.scss';
+import Index from './app/index';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
@@ -9,7 +9,7 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Index />
   </React.StrictMode>
 );
 
